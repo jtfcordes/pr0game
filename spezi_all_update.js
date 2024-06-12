00fcf989981d8052a6d91b608a21858efe209e3c
Jonas Cordes <j.cordes@hs-mannheim.de>
2024-06-12 09:28:57 +0200

    dasd

--- spezi_all_update.js
+++ spezi_all_update.js
@@ -7,28 +7,62 @@
 // @include        /https:\/\/(www.|)pr0game\.com\/(.*\/)?game\.php.*/
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
-// @require      https://raw.githubusercontent.com/jtfcordes/pr0game/main/spezi_all.js
-// ==/UserScript==
 // @require      file:///home/jtfc/Documents/Projekte/pr0Game/Scripts/git/pr0game/spezi_all.js
+// ==/UserScript==
+// @require      https://raw.githubusercontent.com/jtfcordes/pr0game/main/spezi_all.js
 
 
 (function () {
     'use strict';
-    let raids = ["2:210:8", "2:210:9"]
+    let raids = ["2:214:6", "2:214:7", "2:193:4"]
     main(raids)
+    
+    console.log("Spezi Script loaded");
+    // var config = localStorage.getItem("CONFIG")
+    // if( config == null) {
+    //     localStorage.setItem("CONFIG", JSON.stringify({
+    //         "fleet": {
+    //             "raid": {
+    //                 "2:214:6": {
+    //                     "min": 0,
+    //                     "max": 0,
+    //                     "time": 0
+    //                 },
+    //                 "2:214:7": {
+    //                     "min": 0,
+    //                     "max": 0,
+    //                     "time": 0
+    //                 }
+    //             }
+    //         }
+            
+    //     }));
+    //     config = localStorage.getItem("CONFIG")
+    // }
 
+    // console.log("config:",config);
 
     let hasFleet = document.getElementById("fleetTable");
-    if (hasFleet) fleet();
+    if (hasFleet) 
+        {
+            fleet();
+            // replaceCommons(hasFleet.getElementsByTagName("*"))
+            
+        }
 
     let hasStats = document.getElementById("statistics");
     if (hasStats) stats();
 
     let warning_mins = 5.0;
     let hasOverview = document.getElementById("overview");
-    if (hasOverview) overview(warning_mins, true);
+    if (hasOverview) 
+    {
+        overview(warning_mins, true);
+        // replaceCommons(hasOverview.getElementsByTagName("*"))
+    }
 
 
-    replaceSkinText(true);
+    // replaceSkinText(false);
+    // replaceCommons()
     
 })();
